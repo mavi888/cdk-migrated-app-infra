ba6a0d34fc79c49214d62b1b401554a35db78cc5
Villalba <lmv@f84d899822fc.ant.amazon.com>
2022-07-08 10:44:59 +0300

    add dev example

---
+++ backend-app/server/config/dev_example.js
@@ -0,0 +1,7 @@
+module.exports = {
+	mongoURI:
+		'mongodb+srv://<username>:<password>@<clustername>.debkm.mongodb.net',
+	stage: 'local',
+	userPoolId: '<userpoolid>',
+	userPoolClientId: '<userpoolclientid>',
+};
